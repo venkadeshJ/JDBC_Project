1db3ac1fc504e52f3b87f6c4a2e6d851f4230197
venkadeshJ <venkadeshjpm@gmail.com>
2021-09-02 18:19:35 +0530

    JDBC_Project

---
+++ src/com/prodian/Jdbc.java
@@ -0,0 +1,49 @@
+package com.prodian;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Jdbc {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		
+		//1)create a connection
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost: 3306/sakila","root","root");
+		
+		//2)create a statement/query
+		Statement stmt = con.createStatement();
+		
+		//String s = "INSERT INTO ACTOR VALUES(201,'TONY','STARK','2006-02-15 04:34:33')"; //insert query
+		
+		//String s = "UPDATE ACTOR SET FIRST_NAME='THOR', LAST_NAME='THUNDER' WHERE ACTOR_ID=201"; //Update query
+		
+		//String s = "DELETE FROM ACTOR WHERE ACTOR_ID=201"; //Delete query
+		
+		String s="select first_name from actor";
+		
+		
+		
+		//3)Execute Statement/query
+		ResultSet re=stmt.executeQuery(s);
+		
+		while(re.next()) {
+			String fname=re.getString("FIRST_NAME");
+			System.out.println(fname);
+			
+		}
+		
+		
+		
+		//close the connection
+		
+		con.close();
+		
+		System.out.println("Query executed...");
+		
+		
+	}
+
+}
